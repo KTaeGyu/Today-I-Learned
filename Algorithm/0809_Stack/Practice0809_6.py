ed92516db0cac6f5beacfdf8beb98debf6a217ed
KTG <xorb269@naver.com>
2023-08-09 23:31:38 +0900

    0809H

--- Algorithm/0809_Stack/Practice0809_6.py
+++ Algorithm/0809_Stack/Practice0809_6.py
@@ -14,13 +14,47 @@ B. 같은 정수는 2번까지 사용할 수 있다.
 """
 N = int(input())
 path = [1]*N
+cnt = 0
+comb_lst = []
 
-while path[0] != N:
-    if path[len(path)-1] == 1:
-        path[len(path)-2] += path.pop(len(path)-1)
+while True:
+    tem_path = path.copy()
+    # 조건 B 설정
+    cnt_dict = {}
+    for i in path:
+        if i not in cnt_dict:
+            cnt_dict[i] = 1
+        else:
+            cnt_dict[i] += 1
+    tem_cnt = 0
+    for i in cnt_dict.values():
+        if i > 2:
+            tem_cnt = 0
+            break
+        else:
+            tem_cnt += 1
+    
+    # 조건 A, B 적용
     else:
-        temp = path.pop(len(path)-1)
-        for
+        if tem_cnt != 0 and sorted(tem_path) not in comb_lst:
+            cnt += 1
+
+    # 조건 A 설정
+    if sorted(tem_path) not in comb_lst:
+        comb_lst.append(sorted(tem_path))
+
+    if path[0] == N:
+        break
+    elif path[0] == 1:
+        temp = path.pop(0)
+        path[0] += temp
+    else:
+        temp = path.pop(0)
+        path[0] += 1
+        for i in range(1, temp):
+            path.insert(0, 1)
+
+print(cnt)
 
 
 """
